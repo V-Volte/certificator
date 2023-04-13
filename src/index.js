f42aaee7acf258f47b7e04e318eb85239a5a15c1
Sekhara Pramod <32711469+V-Volte@users.noreply.github.com>
2023-04-14 01:08:16 +0530

    Added details as a command line argument, and improved default template. Added multithreading.

--- src/index.js
+++ src/index.js
@@ -1,27 +1,56 @@
 const htmlToImage = require('node-html-to-image');
 const fs = require('fs');
+const {
+    argv,
+    exit
+} = require('process');
 
 const config = JSON.parse(fs.readFileSync(__dirname + '/config.json'));
+
+const inputs = argv[2];
 
 let template = config['template'];
 let html = template['html'];
 let styles = template['styles'];
 
-console.log(html);
 
 let styleString = `<style> ${fs.readFileSync(styles[0])} </style>`;
 
 htmlText = fs.readFileSync(html).toString();
 
 htmlText = htmlText.replace('{{styles}}', styleString);
+let JSONObj;
+try {
+    JSONObj = JSON.parse(fs.readFileSync(inputs));
+} catch (err) {
+    console.error("Please provide a valid JSON file that matches your template as input.");
+    exit(1);
+}
 
-htmlToImage({
-    html: htmlText,
-    content: {
-        Name: "Hello",
-        Age: "World",
-    },
-    output: __dirname + '/outputs.png'
-}).then(() => console.log('The image was created successfully!'));
+let details = [{
+    "Name": "John Doe",
+    "Section": "A",
+}]
 
-console.log(styles);
+let content = {
+    ...JSONObj,
+    ...details[0]
+}
+
+promises = []
+
+for (const detail of details) {
+    let content = {
+        ...JSONObj,
+        ...detail,
+    }
+    promises.push(htmlToImage({
+        html: htmlText,
+        content: content,
+        output: __dirname + '/' + content['Name'].replace(/\s/g, "") + '.png',
+    }));
+}
+
+Promise.all(promises).then(() => {
+    console.log("Done");
+});
